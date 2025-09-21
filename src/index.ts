31e2eb65474f757bd4bad1fbd60b4dca86b9ed24
Alexey Zaitsev <apirator.framework@gmail.com>
2025-09-21 14:55:00 +0200

    Exposes `DiDiscoveryService` and bumps version
    Exports the `DiDiscoveryService` to make it available for use in other modules.
    
    Also, increments the package version to 2.2.1 to reflect the changes.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { DiConfigurator } from "./di-configurator";
 import { DiContainer } from "./di-container";
 import { DiModule } from "./di-module";
+import { DiDiscoveryService } from "./di-discovery-service";
 
 export * from "./types";
 export * from "./errors";
 
-export { DiContainer, DiConfigurator, DiModule };
+export { DiContainer, DiConfigurator, DiModule, DiDiscoveryService };
